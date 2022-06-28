4d7e49cd934a3f112d5e378bc8bc99d5568de092
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 15:38:37 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:38:37 up 3 days, 16:11,  1 user,  load average: 0.33, 0.32, 0.28

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -87,9 +87,7 @@ extern "C" void init_disasm(const char *triple)
   gIP = target->createMCInstPrinter(llvm::Triple(gTriple),
                                     AsmInfo->getAssemblerDialect(), *AsmInfo, *gMII, *gMRI);
   gIP->setPrintImmHex(true);
-#if LLVM_VERSION_MAJOR >= 11
   gIP->setPrintBranchImmAsAddress(true);
-#endif
 }
 
 extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte)
