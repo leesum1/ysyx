a953f077bcf25ad213fb8eaee46f765c6ffebbf3
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 23:59:44 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:59:44 up 4 days, 32 min,  1 user,  load average: 0.41, 0.30, 0.23

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -91,7 +91,7 @@ extern "C" void disassemble(char* str, int size, uint64_t pc, uint8_t * code, in
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
-  gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
+  //gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
 
   std::string s;
   raw_string_ostream os(s);
