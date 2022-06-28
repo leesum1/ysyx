972385afee4213b5b1b91a996bf00ae93aeaa626
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 23:53:53 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:53:53 up 4 days, 27 min,  1 user,  load average: 0.40, 0.29, 0.22

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -51,13 +51,14 @@ static void exec_once(Decode* s, vaddr_t pc) {
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char* p = s->logbuf;
-  p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
+  //p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
   int i;
   uint8_t* inst = (uint8_t*)&s->isa.inst.val;
-  // for (i = ilen - 1; i >= 0; i--) {
-  //   p += snprintf(p, 4, " %02x", inst[i]);
-  // }
+  /* 当前指令 */
+  for (i = ilen - 1; i >= 0; i--) {
+    p += snprintf(p, 4, " %02x", inst[i]);
+  }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0)
