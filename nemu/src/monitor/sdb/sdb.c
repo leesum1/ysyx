8942762fdd9f22f2077bf83f8a5bbdf9feb9a167
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 23:23:44 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:23:44 up 3 days, 23:56,  1 user,  load average: 0.52, 0.39, 0.28

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -104,7 +104,7 @@ static int cmd_x(char* args) {
   DEBUG_S("len:%d,addr:%x\n", len, addr);
   int32_t i = 0;
   for (i = 0; i < len; i++) {
-    DEBUG_S("addr:0x%08x\tData: 0x%016p\n", addr,
+    DEBUG_S("addr:0x%08x\tData: 0x%16p\n", addr,
       (void*)vaddr_read(addr, 8));
     addr += 8;
   }
