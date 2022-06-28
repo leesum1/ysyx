2acd6e31fa6b54c3fe97b1488943c40d9c71d649
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 16:39:36 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:39:36 up 3 days, 17:12,  1 user,  load average: 0.17, 0.20, 0.18

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -21,6 +21,6 @@ int is_exit_status_bad()
 {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
              (nemu_state.state == NEMU_QUIT);
-  printf("good:%d", good);
-  return -2;
+  printf("good:%d", !good);
+  return !good;
 }
