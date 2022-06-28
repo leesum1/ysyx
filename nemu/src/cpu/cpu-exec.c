3b017e49b95879872ef98f8e6c50227396e5168d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 23:44:36 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:44:36 up 4 days, 17 min,  1 user,  load average: 0.24, 0.18, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -67,8 +67,8 @@ static void exec_once(Decode* s, vaddr_t pc) {
   p += space_len;
 
   void disassemble(char* str, int size, uint64_t pc, uint8_t * code, int nbyte);
-  // disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
-  //   MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t*)&s->isa.inst.val, ilen);
+  disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
+    MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t*)&s->isa.inst.val, ilen);
 #endif
 }
 
