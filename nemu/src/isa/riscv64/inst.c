b5e021c0776c9802c5a76d9fa90185ef21f9da2f
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-28 23:45:50 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:45:50 up 4 days, 19 min,  1 user,  load average: 0.19, 0.18, 0.19

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -38,20 +38,20 @@ static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
 
-static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
+static void decode_operand(Decode* s, word_t* dest, word_t* src1, word_t* src2, int type) {
   uint32_t i = s->isa.inst.val;
-  int rd  = BITS(i, 11, 7);
+  int rd = BITS(i, 11, 7);
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
   destR(rd);
   switch (type) {
-    case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
-    case TYPE_U: src1I(immU(i)); break;
-    case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+  case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
+  case TYPE_U: src1I(immU(i)); break;
+  case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
   }
 }
 
-static int decode_exec(Decode *s) {
+static int decode_exec(Decode* s) {
   word_t dest = 0, src1 = 0, src2 = 0;
   s->dnpc = s->snpc;
 
@@ -61,21 +61,21 @@ static int decode_exec(Decode *s) {
   __VA_ARGS__ ; \
 }
 
-  INSTPAT_START();
-  INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
-  INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
-  INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
+  // INSTPAT_START();
+  // INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
+  // INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
+  // INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
 
-  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
-  INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
-  INSTPAT_END();
+  // INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  // INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
+  // INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
 
   return 0;
 }
 
-int isa_exec_once(Decode *s) {
+int isa_exec_once(Decode* s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
   return decode_exec(s);
 }
