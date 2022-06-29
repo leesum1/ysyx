6887f047a242898abb135cb8c0c302772c58b3d7
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 20:59:38 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:59:38 up 4 days, 21:32,  1 user,  load average: 0.17, 0.13, 0.09

--- nemu/src/monitor/sdb/exprresult.cc
+++ nemu/src/monitor/sdb/exprresult.cc
@@ -4,36 +4,49 @@
 
 using namespace std;
 
-class exprresult {
+class Exprresult {
 private:
     typedef struct token {
         int type;
         char str[32];
     } Token;
     vector<Token> tokens;
+    enum {
+        TK_NOTYPE = 256, TK_EQ,
+        TK_NUM,
+        /* TODO: Add more token types */
+
+    };
+
+    stack<uint32_t> stackOpre, stackNum;
 
 public:
-    exprresult(void* tokens_addr, int num);
-    ~exprresult();
+    Exprresult(void* tokens_addr, int num);
+    ~Exprresult();
     void printTokens();
+    bool isOperator(Token val);
+    bool isPriority(Token val);
 };
 
 
-exprresult::exprresult(void* tokens_addr, int num) {
+
+
+Exprresult::Exprresult(void* tokens_addr, int num) {
     Token* p = (Token*)tokens_addr;
     for (int i = 0; i < num; i++) {
         tokens.push_back(p[i]);
     }
     printTokens();
+    isPriority(tokens.at(0));
 }
 
-exprresult::~exprresult() {
-    cout << "exprresult end" << endl;
+Exprresult::~Exprresult() {
+    cout << "Exprresult end" << endl;
 }
 
-void exprresult::printTokens() {
+void Exprresult::printTokens() {
 
-    cout << "exprresult start" << endl;
+    cout << "Exprresult start" << endl;
     for (int i = 0; i < tokens.size(); i++) {
         cout << " str: " << tokens.at(i).str \
             << " type: " << tokens.at(i).type << endl;
@@ -41,10 +54,59 @@ void exprresult::printTokens() {
 
 }
 
+bool Exprresult::isOperator(Token val) {
 
+    if (val.type != this->TK_NUM) {
+        return true;
+    }
+    return false;
+
+}
+
+/**
+ * @brief () * / + -
+ *
+ * @param val
+ * @return true
+ * @return false
+ */
+bool Exprresult::isPriority(Token val) {
+
+    if (val.type == this->TK_NUM) {
+        cout << "please input opra!!!!" << endl;
+        return false;
+    }
+    bool ret;
+    uint32_t toptype;
+    switch (val.type) {
+    case '(':
+        ret = true;
+        break;
+        // case ')':
+        //     break;
+    case '*':
+    case '/':
+        toptype = stackOpre.top();
+        if ('+' == toptype || '-' == toptype) {
+            ret = true;
+        }
+        break;
+    case '+':
+    case '-':
+        ret = true;
+        break;
+    default:
+        cout << "type undefine!!!!!" << endl;
+        break;
+    }
+    return ret;
+}
+
+
+/* 供外部调用 */
 extern "C" void exprcpp(void* tokens_addr, int num) {
 
-    exprresult test(tokens_addr, num);
+    Exprresult test(tokens_addr, num);
 }
 
 
