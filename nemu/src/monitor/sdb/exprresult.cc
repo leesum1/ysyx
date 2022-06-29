7dddf97bfb5f19b995c9cc36d86f997253e73452
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 23:22:15 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:22:15 up 4 days, 23:55,  1 user,  load average: 0.11, 0.09, 0.09

--- nemu/src/monitor/sdb/exprresult.cc
+++ nemu/src/monitor/sdb/exprresult.cc
@@ -114,12 +114,12 @@ bool Exprresult::isOperator(Token val) {
  */
 bool Exprresult::isPriority(Token val) {
 
-    int32_t toptype;
-    toptype = stackOpre.top();
+
     /* 操作数少于两个,或者为括号 */
     if ((stackNum.size() < 2))
         return true;
-
+    int32_t toptype;
+    toptype = stackOpre.top();
     if (toptype == '(')
         return true;
 
