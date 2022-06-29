d7837ecc6ebb0270fd58895d4f774d5af8177c0d
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 19:24:39 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:24:39 up 4 days, 19:57,  2 users,  load average: 0.13, 0.17, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -110,6 +110,7 @@ static bool make_token(char* e) {
           nr_token++;
         }
         extern void exprcpp(void* tokens_addr, int num);
+        exprcpp(tokens, nr_token);
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
