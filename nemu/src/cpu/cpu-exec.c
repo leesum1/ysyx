fdf6ecf9ce089e711838462e2616fb0854b7b007
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 13:39:15 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:39:15 up 4 days, 14:12,  2 users,  load average: 0.25, 0.17, 0.08

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -88,9 +88,10 @@ static void exec_once(Decode* s, vaddr_t pc) {
 static void execute(uint64_t n) {
   Decode s;
   for (; n > 0; n--) {
+    trace_and_difftest(&s, cpu.pc);
     exec_once(&s, cpu.pc);
     g_nr_guest_inst++;
-    trace_and_difftest(&s, cpu.pc);
+
     if (nemu_state.state != NEMU_RUNNING)
       break;
     IFDEF(CONFIG_DEVICE, device_update());
