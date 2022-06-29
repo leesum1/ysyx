d1bf330b3c2dfe03ec79b0cf3fc3206db5faceaa
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 14:24:36 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:24:36 up 4 days, 14:57,  2 users,  load average: 0.04, 0.07, 0.05

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -22,7 +22,7 @@ static const uint32_t img[] = {
     0x00000297, // auipc t0,0 ,    (pc+0) 写入 t0
     0x0102b503, // ld  a0,16(t0)    a0 写 (t0+16)8byte
     0x0002b823, // sd  zero,16(t0) (t0+16)8byte 内存清0
-
+    0x0102b503, // ld  a0,16(t0)    a0 写 (t0+16)8byte
     0x00100073, // ebreak (used as nemu_trap)
     0xdeadbeef, // some data
 };
