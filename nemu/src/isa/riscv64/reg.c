2b7940ede8730bb30d1189bdf7ae1dc8e993ced9
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 14:31:05 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:31:05 up 4 days, 15:04,  2 users,  load average: 0.09, 0.07, 0.05

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -17,21 +17,19 @@
 #include "local-include/reg.h"
 #include <string.h>
 
-const char *regs[] = {
+const char* regs[] = {
     "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
     "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
     "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"};
+    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6" };
 
-void isa_reg_display()
-{
-  for (int i = 0; i < 32; i++)
-  {
+void isa_reg_display() {
+  for (int i = 0; i < 32; i++) {
     printf("%d:%s\t%lx\n", i, reg_name(i, 64), gpr(i));
   }
+  printf("%d:%s\t%lx\n", 33, "pc", cpu.pc);
 }
 
-word_t isa_reg_str2val(const char *s, bool *success)
-{
+word_t isa_reg_str2val(const char* s, bool* success) {
   return 0;
 }
