fce7a6ece6f2013133fd91babf16325ef9cd5aa7
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-29 18:50:51 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:50:51 up 4 days, 19:24,  2 users,  load average: 0.07, 0.16, 0.10

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -25,6 +25,8 @@ INCLUDES = $(addprefix -I, $(INC_PATH))
 CFLAGS  := -O2 -MMD -Wall  $(INCLUDES) $(CFLAGS) #-Werror 不需要
 LDFLAGS := -O2 $(LDFLAGS)
 
+CXXSRC += $(WORK_DIR)/src/monitor/sdb/expr.cc
+
 OBJS = $(SRCS:%.c=$(OBJ_DIR)/%.o) $(CXXSRC:%.cc=$(OBJ_DIR)/%.o)
 
 # Compilation patterns
