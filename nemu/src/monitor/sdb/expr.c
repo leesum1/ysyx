45c0eadda83f520205cc94f33db46d4a26a632fc
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-30 16:23:02 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:02 up 5 days, 16:56,  1 user,  load average: 0.25, 0.14, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -109,15 +109,6 @@ static bool make_token(char* e) {
             tokens[nr_token].type, nr_token);
           nr_token++;
         }
-
-        /* TODO: Now a new token is recognized with rules[i]. Add codes
-         * to record the token in the array `tokens'. For certain types
-         * of tokens, some extra actions should be performed.
-         */
-
-         // switch (rules[i].token_type) {
-         // default: TODO();
-         // }
         break;
       }
     }
