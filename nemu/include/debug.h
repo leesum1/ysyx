8452b1859f50bc740cd76df5b83dbc3ec21b813d
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-01 21:27:59 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:27:59 up 6 days, 22:01,  2 users,  load average: 0.06, 0.09, 0.08

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -19,11 +19,11 @@
 #include <common.h>
 #include <stdio.h>
 #include <utils.h>
-extern void assert_fail_msg();
+
 #define Log(format, ...)                                  \
   _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
        __FILE__, __LINE__, __func__, ##__VA_ARGS__)
-//完整的调试信息：设备类型，当前源文件名，当前源函数名，当前源代码行号，格式化输出内容，格式化参数
+ //完整的调试信息：设备类型，当前源文件名，当前源函数名，当前源代码行号，格式化输出内容，格式化参数
 
 #define DEBUG_L(format, argent...)  printf("Node:S, File: "__FILE__                \
                                           ", Func: %s(), Line: %03d: " format "", \
@@ -33,6 +33,8 @@ extern void assert_fail_msg();
 //最简洁的调试信息：格式化输出内容，格式化参数
 #define DEBUG_S(format, argent...) printf("" format "", ##argent)
 
+
+extern void assert_fail_msg();
 #define Assert(cond, format, ...)                                                                   \
   do                                                                                                \
   {                                                                                                 \
