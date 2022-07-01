eeafac68ec928943ec05016fbbce0f8a4a6b2e6f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-01 19:34:15 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:34:15 up 6 days, 20:07,  2 users,  load average: 0.08, 0.10, 0.09

--- nemu/src/monitor/sdb/exprresult.cc
+++ nemu/src/monitor/sdb/exprresult.cc
@@ -93,7 +93,7 @@ void Exprresult::printTokens() {
 
 uint64_t Exprresult::run1() {
     bool ret;
-    for (int i = 0; i < tokens.size(); i++) {
+    for (size_t i = 0; i < tokens.size(); i++) {
         /* 跳过无用数据 */
         if (tokens.at(i).type == TK_NOTYPE) {
             continue;
