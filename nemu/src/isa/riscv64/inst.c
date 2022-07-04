19177b949368eb60c739547d48068b46af366a07
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-04 11:47:57 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:47:57 up 9 days, 12:21,  2 users,  load average: 0.40, 0.49, 0.28

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -73,14 +73,21 @@ static void decode_operand(Decode* s, word_t* dest, word_t* src1, word_t* src2, 
   int rs2 = BITS(i, 24, 20);
   destR(rd);
   switch (type) {
+    // case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
+    // case TYPE_U: src1I(immU(i)); break;
+    // case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    //   /* add by leesum */
+    // case TYPE_J: src1I(immJ(i));break;
+    // case TYPE_R: src1R(rs1); src2R(rs2); break;
+    // case TYPE_B: destI(immB(i));src1R(rs1); src2R(rs2); break;
+    // default: break;
+
+  case TYPE_R: src1R(rs1);     src2R(rs2);     break;
   case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
+  case TYPE_S: destI(immS(i)); src1R(rs1);     src2R(rs2); break;
+  case TYPE_B: destI(immB(i)); src1R(rs1);     src2R(rs2); break;
   case TYPE_U: src1I(immU(i)); break;
-  case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
-    /* add by leesum */
-  case TYPE_J: src1I(immJ(i));break;
-  case TYPE_R: src1R(rs1); src2R(rs2); break;
-  case TYPE_B: destI(immB(i));src1R(rs1); src2R(rs2); break;
-  default: break;
+  case TYPE_J: src1I(immJ(i)); break;
   }
 
 }
