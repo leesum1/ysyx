14f4119d93903fea1ead382bc1ef08bb0996221f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-04 11:49:52 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:49:52 up 9 days, 12:23,  2 users,  load average: 0.35, 0.43, 0.28

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -47,16 +47,16 @@ static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i
 //     BITS(i, 11, 8)) << 1 |
 //     0, 13);
 // }
-static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1); }
+//static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1); }
 static word_t immB(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1); }
-// static word_t immJ(uint32_t i) {
-//   return SEXT((
-//     BITS(i, 31, 31) << 20 |
-//     BITS(i, 19, 12) << 12 |
-//     BITS(i, 20, 20) << 11 |
-//     BITS(i, 30, 21) << 1 |
-//     0), 21);
-// }
+static word_t immJ(uint32_t i) {
+  return SEXT((
+    BITS(i, 31, 31) << 20 |
+    BITS(i, 19, 12) << 12 |
+    BITS(i, 20, 20) << 11 |
+    BITS(i, 30, 21) << 1 |
+    0), 21);
+}
 
 #define S32(i) ((int32_t)i)
 #define S64(i) ((int64_t)i)
