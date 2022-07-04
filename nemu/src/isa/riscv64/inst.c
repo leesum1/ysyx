c08a9556b98aee4f157797041cd0bd14233867bd
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-04 11:55:17 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:55:17 up 9 days, 12:28,  2 users,  load average: 0.20, 0.25, 0.24

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -50,12 +50,12 @@ static word_t immB(uint32_t i) {
 //static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 19, 12) << 12) | (BITS(i, 20, 20) << 11) | (BITS(i, 30, 21) << 1); }
 //static word_t immB(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 7, 7) << 11) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1); }
 static word_t immJ(uint32_t i) {
-  return SEXT((
-    BITS(i, 31, 31) << 20 |
+  return (SEXT(
+    BITS(i, 31, 31), 1) << 20 |
     BITS(i, 19, 12) << 12 |
     BITS(i, 20, 20) << 11 |
     BITS(i, 30, 21) << 1 |
-    0), 21);
+    0);
 }
 
 #define S32(i) ((int32_t)i)
