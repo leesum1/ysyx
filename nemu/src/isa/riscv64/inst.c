18b121049cd2853b02855caff01d59fd97678a69
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-04 13:21:09 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:21:09 up 9 days, 13:54,  1 user,  load average: 0.58, 0.29, 0.16

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -80,13 +80,6 @@ static void decode_operand(Decode* s, word_t* dest, word_t* src1, word_t* src2, 
   case TYPE_R: src1R(rs1); src2R(rs2); break;
   case TYPE_B: destI(immB(i));src1R(rs1); src2R(rs2); break;
   default: break;
-
-    // case TYPE_R: src1R(rs1);     src2R(rs2);     break;
-    // case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
-    // case TYPE_S: destI(immS(i)); src1R(rs1);     src2R(rs2); break;
-    // case TYPE_B: destI(immB(i)); src1R(rs1);     src2R(rs2); break;
-    // case TYPE_U: src1I(immU(i)); break;
-    // case TYPE_J: src1I(immJ(i)); break;
   }
 
 }
