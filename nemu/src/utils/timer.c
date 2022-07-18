ae9ae1190e2a0f240549df76f0d530c8c6a024a5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 19:01:49 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:01:49 up 17 min,  1 user,  load average: 0.24, 0.33, 0.42

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -17,9 +17,9 @@
 #include MUXDEF(CONFIG_TIMER_GETTIMEOFDAY, <sys/time.h>, <time.h>)
 
 IFDEF(CONFIG_TIMER_CLOCK_GETTIME,
-    static_assert(CLOCKS_PER_SEC == 1000000, "CLOCKS_PER_SEC != 1000000"));
+  static_assert(CLOCKS_PER_SEC == 1000000, "CLOCKS_PER_SEC != 1000000"));
 IFDEF(CONFIG_TIMER_CLOCK_GETTIME,
-    static_assert(sizeof(clock_t) == 8, "sizeof(clock_t) != 8"));
+  static_assert(sizeof(clock_t) == 8, "sizeof(clock_t) != 8"));
 
 static uint64_t boot_time = 0;
 
@@ -29,7 +29,7 @@ static uint64_t get_time_internal() {
 #elif defined(CONFIG_TIMER_GETTIMEOFDAY)
   struct timeval now;
   gettimeofday(&now, NULL);
-  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+  uint64_t us = now.tv_sec * 1000000 + (now.tv_usec + 500);
 #else
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
