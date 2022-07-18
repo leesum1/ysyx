42cadb15b260a6824f54369a8bbddfcc18274c93
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 14:25:45 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:25:45 up 12 days, 22:16,  2 users,  load average: 0.50, 0.49, 0.45

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -9,9 +9,9 @@
 extern "C" {
 #endif
 
-#define __NATIVE_USE_KLIB__ 1
+  // #define __NATIVE_USE_KLIB__ 1
 
-  // string.h
+    // string.h
   void* memset(void* s, int c, size_t n);
   void* memcpy(void* dst, const void* src, size_t n);
   void* memmove(void* dst, const void* src, size_t n);
