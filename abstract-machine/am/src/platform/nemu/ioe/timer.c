67e571caaf18012d254f03ef7f0b25e41c0f5d8f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 14:47:04 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:47:04 up 12 days, 22:37,  2 users,  load average: 0.35, 0.24, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,8 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T* uptime) {
-  putstr("test\n");
-  uptime->us = 0;
+  AM_TIMER_UPTIME_T t = io_read(AM_TIMER_UPTIME);
+  uptime->us = t.us;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T* rtc) {
