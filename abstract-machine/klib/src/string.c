2d0297a7b92cff74e2cf31bf2135e1922a767f6f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 13:21:44 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:21:44 up 12 days, 21:12,  1 user,  load average: 0.05, 0.16, 0.08

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,89 +4,116 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-/*长度不包含'\0'*/
-size_t strlen(const char* s) {
-  size_t len = 0;
-  while (*s != '\0') {
-    len++;
-    s++;
-  }
-  return len;
+size_t strnlen(const char* s, size_t count) {
+  const char* sc;
+  for (sc = s; count-- && *sc != '\0'; ++sc)
+    /* nothing */;
+  return sc - s;
 }
 
-char* strcpy(char* dst, const char* src) {
-  char* address = dst;
-  int size = strlen(dst) + 1;
-  if ((dst > src) && (dst <= src + size - 1)) //内存重叠，从尾往前复制
-  {
-    src = src + size - 1;
-    dst = dst + size - 1;
-    while (size--) {
-      *dst-- = *src--;
-    }
+size_t strlen(const char* s) {
+  size_t count = 0;
+  for (;*s != '\0'; s++) {
+    count++;
   }
-  else//正常情况，从头往后复制
-  {
-    while (size--) {
-      *dst++ = *src++;
-    }
+  return count;
+}
+
+char* strcpy(char* dst, const char* src) { //test done.
+  char* ret = dst;
+  for (; *src != '\0'; src++, dst++) {
+    *dst = *src;
   }
-  return address;
+  *dst = '\0';
+  return ret;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  panic("Not implemented");
-}
-
-char* strcat(char* dst, const char* src) {
-  assert(dst != NULL && src != NULL);
-  char* temp = dst;
-  while (*temp != '\0')
-    temp++;
-  while ((*temp++ = *src++) != '\0');
-  return dst;
-}
-
-int strcmp(const char* s1, const char* s2) {
-  while ((*s1 != '\0') && (*s1 == *s2)) {
-    s1++;
-    s2++;
+  char* ret = dst;
+  for (size_t i = 0; i < n;i++) {
+    *(dst + i) = *(src + i);
   }
-  int t;
-  t = *s1 - *s2;
-  return t;
+  *(dst + n) = '\0';
+  return ret;
+}
+
+char* strcat(char* dst, const char* src) {  //test done.
+  char* ret = dst;
+  for (; *dst != '\0'; dst++); // dst -> '\0'
+  strcpy(dst, src);
+  return ret;
+}
+
+int strcmp(const char* s1, const char* s2) {  //test done.
+  int ret = 0;
+  for (; *s1 != '\0' && *s2 != '\0'; s1++, s2++) {
+    ret = *s1 - *s2;
+    if (ret != 0)
+      return ret; //can not be break!
+  }
+  ret = *s1 - *s2; //check for finish '\0'.
+  return ret;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  panic("Not implemented");
+  int ret = 0;
+  for (int i = 0; i < n;i++) {
+    ret = *(s1 + i) - *(s2 + i);
+    if (ret != 0)
+      break;
+  }
+  return ret;
 }
 
-void* memset(void* s, int c, size_t n) {
-  assert(s);
-  void* temp = s;
-  while (n--) {
-    *(char*)s = (char)c;
-    s = (char*)s + 1;
+void* memset(void* s, int c, size_t n) { //test done.
+  unsigned char* p = (unsigned char*)s;
+  for (size_t i = 0;i < n;i++) {
+    *(p + i) = c;
   }
-  return temp;
+  return s;
 }
 
 void* memmove(void* dst, const void* src, size_t n) {
-  panic("Not implemented");
+  // this function can be uesd for overlapping areas.
+  if (dst <= src) {
+    // ----|dst----------|src---------|----------
+    char* pdst = (char*)dst;
+    char* psrc = (char*)src;
+    for (size_t i = 0; i < n; i++) {
+      *pdst++ = *psrc++;
+    }
+  }
+  else {
+    // ----|src----------|dst---------|----------
+    char* pdst = (char*)dst + n;
+    char* psrc = (char*)src + n;
+    for (size_t i = 0; i < n; i++) {
+      *--pdst = *--psrc;
+    }
+  }
+  return dst;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  panic("Not implemented");
+  // this function can not be uesd for overlapping areas!
+  char* pout = (char*)out;
+  const char* pin = (char*)in;
+  for (size_t i = 0; i < n; i++) {
+    *pout++ = *pin++;
+  }
+  return out;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n) {
-  char* arr1 = (char*)s1;
-  char* arr2 = (char*)s2;
-  while (--n && *arr1 && *arr1 == *arr2) {
-    arr1++;
-    arr2++;
+int memcmp(const void* s1, const void* s2, size_t n) { //test done.
+  int ret = 0;
+  const unsigned char* p1 = s1;
+  const unsigned char* p2 = s2;
+  for (int i = 0; i < n;i++) {
+    ret = *(p1 + i) - *(p2 + i);
+    if (ret != 0)
+      break;
   }
-  return *arr1 - *arr2;
+  return ret;
 }
 
 #endif
