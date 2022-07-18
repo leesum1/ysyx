5b8360aba42c62b5f2ad7647fb697d0ffb6f63e8
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-18 13:28:26 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-121-generic #137-Ubuntu SMP Wed Jun 15 13:33:07 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:28:26 up 12 days, 21:18,  1 user,  load average: 0.38, 0.18, 0.11

--- abstract-machine/klib/include/klib.h
+++ abstract-machine/klib/include/klib.h
@@ -9,40 +9,41 @@
 extern "C" {
 #endif
 
-//#define __NATIVE_USE_KLIB__
+  //#define __NATIVE_USE_KLIB__
 
-// string.h
-void  *memset    (void *s, int c, size_t n);
-void  *memcpy    (void *dst, const void *src, size_t n);
-void  *memmove   (void *dst, const void *src, size_t n);
-int    memcmp    (const void *s1, const void *s2, size_t n);
-size_t strlen    (const char *s);
-char  *strcat    (char *dst, const char *src);
-char  *strcpy    (char *dst, const char *src);
-char  *strncpy   (char *dst, const char *src, size_t n);
-int    strcmp    (const char *s1, const char *s2);
-int    strncmp   (const char *s1, const char *s2, size_t n);
+  // string.h
+  void* memset(void* s, int c, size_t n);
+  void* memcpy(void* dst, const void* src, size_t n);
+  void* memmove(void* dst, const void* src, size_t n);
+  int    memcmp(const void* s1, const void* s2, size_t n);
+  size_t strlen(const char* s);
+  size_t strnlen(const char* s, size_t count);
+  char* strcat(char* dst, const char* src);
+  char* strcpy(char* dst, const char* src);
+  char* strncpy(char* dst, const char* src, size_t n);
+  int    strcmp(const char* s1, const char* s2);
+  int    strncmp(const char* s1, const char* s2, size_t n);
 
-// stdlib.h
-void   srand     (unsigned int seed);
-int    rand      (void);
-void  *malloc    (size_t size);
-void   free      (void *ptr);
-int    abs       (int x);
-int    atoi      (const char *nptr);
+  // stdlib.h
+  void   srand(unsigned int seed);
+  int    rand(void);
+  void* malloc(size_t size);
+  void   free(void* ptr);
+  int    abs(int x);
+  int    atoi(const char* nptr);
 
-// stdio.h
-int    printf    (const char *format, ...);
-int    sprintf   (char *str, const char *format, ...);
-int    snprintf  (char *str, size_t size, const char *format, ...);
-int    vsprintf  (char *str, const char *format, va_list ap);
-int    vsnprintf (char *str, size_t size, const char *format, va_list ap);
+  // stdio.h
+  int    printf(const char* format, ...);
+  int    sprintf(char* str, const char* format, ...);
+  int    snprintf(char* str, size_t size, const char* format, ...);
+  int    vsprintf(char* str, const char* format, va_list ap);
+  int    vsnprintf(char* str, size_t size, const char* format, va_list ap);
 
-// assert.h
+  // assert.h
 #ifdef NDEBUG
-  #define assert(ignore) ((void)0)
+#define assert(ignore) ((void)0)
 #else
-  #define assert(cond) \
+#define assert(cond) \
     do { \
       if (!(cond)) { \
         printf("Assertion fail at %s:%d\n", __FILE__, __LINE__); \
