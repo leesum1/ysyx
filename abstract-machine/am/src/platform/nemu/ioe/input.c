e6b37c14f03626772b507b35469c495cb89e591b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 17:21:16 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:16 up 17:45,  2 users,  load average: 0.92, 1.08, 1.19

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,19 +3,18 @@
 #include <stdio.h>
 #define KEYDOWN_MASK 0x8000
 
-static AM_INPUT_KEYBRD_T read_key(void) {
-  AM_INPUT_KEYBRD_T kb;
-  uint32_t kbVal = inl(KBD_ADDR);
-  printf("key:%d\n", kbVal);
-  kb.keydown = (KEYDOWN_MASK & kbVal) >> 31;
-  kb.keycode = (~KEYDOWN_MASK) & kbVal;
-  return kb;
-}
+// static AM_INPUT_KEYBRD_T read_key(void) {
+//   AM_INPUT_KEYBRD_T kb;
+//   uint32_t kbVal = inl(KBD_ADDR);
+//   printf("key:%d\n", kbVal);
+//   kb.keydown = (KEYDOWN_MASK & kbVal) >> 31;
+//   kb.keycode = (~KEYDOWN_MASK) & kbVal;
+//   return kb;
+// }
 
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T* kbd) {
-  AM_INPUT_KEYBRD_T tempkb = read_key();
-
-  kbd->keydown = tempkb.keydown;
-  kbd->keycode = tempkb.keycode;
+  int code = inl(KBD_ADDR);
+  kbd->keydown = (code & KEYDOWN_MASK ? true : false);
+  kbd->keycode = code & ~KEYDOWN_MASK;
 }
