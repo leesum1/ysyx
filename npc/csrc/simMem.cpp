2212f494f552f8907cde4d83b335850afe36ba45
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-23 19:31:13 +0800

    > sim RTL ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:31:13 up 4 days, 19:55,  1 user,  load average: 0.93, 0.71, 0.50

--- npc/csrc/simMem.cpp
+++ npc/csrc/simMem.cpp
@@ -90,24 +90,23 @@ bool SimMem::in_pmem(paddr_t addr) {
 }
 
 void SimMem::out_of_bound(paddr_t addr) {
-
+    //cout << "addr:\t" << hex << addr << " not in pmem!" << endl;
 }
 word_t SimMem::paddr_read(paddr_t addr, int len) {
     if (in_pmem(addr)) {
         return pmem_read(addr, len);
     }
     static uint64_t rtc_time;
-    // if (addr = 0xa0000048) {
-    //     static struct timeval now;
-    //     gettimeofday(&now, NULL);
-    //     long seconds = now.tv_sec;
-    //     long useconds = now.tv_usec;
-    //     rtc_time = (seconds * 1000000 + (useconds + 500));
-    //     return (uint32_t)rtc_time;
-    // }
-    if (addr = 0xa0000048 + 4) {
-        //return 100000;
-        cout << "addr:\t" << hex << addr << " not in pmem!" << endl;
+    if (addr == 0xa0000048) {
+        static struct timeval now;
+        gettimeofday(&now, NULL);
+        long seconds = now.tv_sec;
+        long useconds = now.tv_usec;
+        rtc_time = (seconds * 1000000 + (useconds + 500));
+        return (uint32_t)rtc_time;
+    }
+    else if (addr == 0xa0000048 + 4) {
+        return rtc_time >> 32;
     }
 
     out_of_bound(addr);
