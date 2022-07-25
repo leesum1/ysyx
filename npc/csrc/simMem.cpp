33c4d75b2bf96ad3dd69f98846aa482a7dbce8c0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 22:10:04 +0800

    > sim RTL ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:10:04 up 1 day, 22:26,  2 users,  load average: 0.45, 0.33, 0.29

--- npc/csrc/simMem.cpp
+++ npc/csrc/simMem.cpp
@@ -99,20 +99,21 @@ word_t SimMem::paddr_read(paddr_t addr, int len) {
     if (in_pmem(addr)) {
         return pmem_read(addr, len);
     }
-    /* 读取低位时刷新时间 */
-    static uint64_t rtc_time;
-    static struct timeval now;
+    // /* 读取低位时刷新时间 */
+    // static uint64_t rtc_time;
+    // static struct timeval now;
 
-    if (addr == 0xa0000048) {
-        gettimeofday(&now, NULL);
-        long seconds = now.tv_sec;
-        long useconds = now.tv_usec;
-        rtc_time = (seconds * 1000000 + (useconds + 500));
-        return (uint32_t)rtc_time;
-    }
-    else if (addr == 0xa0000048 + 4) {
-        return rtc_time >> 32;
-    }
+    // if (addr == 0xa0000048) {
+    //     gettimeofday(&now, NULL);
+    //     long seconds = now.tv_sec;
+    //     long useconds = now.tv_usec;
+    //     rtc_time = (seconds * 1000000 + (useconds + 500));
+    //     return (uint32_t)rtc_time;
+    // }
+    // else if (addr == 0xa0000048 + 4) {
+    //     return rtc_time >> 32;
+    // }
+    return Device->read(addr);
 
     out_of_bound(addr);
     return 0;
