761441bea4511eb9afe8dc024341f45d6ca943ef
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 18:21:48 +0800

    > sim RTL ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:21:48 up 1 day, 18:38,  1 user,  load average: 0.72, 0.25, 0.21

--- npc/csrc/include/deviceuart.h
+++ npc/csrc/include/deviceuart.h
@@ -10,7 +10,7 @@ namespace topdevice {
 
     public:
         deviceuart(/* args */);
-        ~deviceuart();
+        virtual  ~deviceuart();
         void write(paddr_t addr, word_t data, uint32_t len);
         word_t read(paddr_t addr);
         void init(const char* name);
