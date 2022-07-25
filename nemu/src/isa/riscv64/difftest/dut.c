dfea492fba3904fddac637ec0210984580f7dd5f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 11:17:38 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:17:38 up 1 day, 11:34,  2 users,  load average: 0.29, 0.22, 0.10

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -17,8 +17,18 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+bool isa_difftest_checkregs(CPU_state* ref_r, vaddr_t pc) {
+  for (int i = 0; i < 32; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i])
+      printf("reg:%d,err!!");
+    return false;
+  }
+  if (ref_r->pc != cpu.pc) {
+    printf("pc,err!!");
+    return false;
+  }
+
+  return true;
 }
 
 void isa_difftest_attach() {
