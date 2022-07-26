2c22c878c7d92ef6ecf989a152c086ec2a36cfb5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 19:54:37 +0800

    > sim RTL ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:54:37 up 2 days, 20:11,  2 users,  load average: 0.26, 0.25, 0.26

--- abstract-machine/am/src/riscv/npc/gpu.c
+++ abstract-machine/am/src/riscv/npc/gpu.c
@@ -5,14 +5,14 @@
 #define H    300  
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 void __am_gpu_init() {
-    // int i;
-    // int w = W;
-    // int h = H;
-    // uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
-    // for (i = 0; i < w * h; i++) {
-    //   fb[i] = i;
-    // }
-    // outl(SYNC_ADDR, 1);
+    int i;
+    int w = W;
+    int h = H;
+    uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;
+    for (i = 0; i < w * h; i++) {
+        fb[i] = i;
+    }
+    outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T* cfg) {
