06a7ce400ecf89ab5b25b17cbdeb9047bca4645f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 14:08:35 +0800

    > sim RTL ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:08:35 up 4 days, 14:25,  3 users,  load average: 0.07, 0.20, 0.25

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -30,9 +30,9 @@ static void* lut[128] = {
 //static void fail(void* buf) { panic("access nonexist register"); }
 
 bool ioe_init() {
-  // for (int i = 0; i < LENGTH(lut); i++)
-  //   // if (!lut[i]) lut[i] = fail;
-  __am_gpu_init();
+  for (int i = 0; i < LENGTH(lut); i++)
+    // if (!lut[i]) lut[i] = fail;
+    __am_gpu_init();
   __am_timer_init();
   return true;
 }
