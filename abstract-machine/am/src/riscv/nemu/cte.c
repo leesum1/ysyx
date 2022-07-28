8c5a2d045a8e8d386d4e9e45d7ffca1e89f16f9b
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 00:00:31 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:00:31 up 5 days, 17 min,  2 users,  load average: 1.44, 0.58, 0.28

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -30,7 +30,6 @@ bool cte_init(Context* (*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
-
   // register event handler
   user_handler = handler;
 
