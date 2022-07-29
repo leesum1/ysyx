734d5132471c5ae855b55a51d2c84494a456c5d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-29 15:02:43 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:02:43 up 5 days, 15:19,  3 users,  load average: 0.21, 0.16, 0.12

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -14,9 +14,9 @@ Context* __am_irq_handle(Context* c) {
     Event ev = { 0 };
     switch (c->mcause) {
     case -1: ev.event = EVENT_YIELD; break;   // yield
-    case  0: ev.event = EVENT_SYSCALL; break; // exit
-    case  1: ev.event = EVENT_SYSCALL; break; // yield
-    case  4: ev.event = EVENT_SYSCALL; break; // write
+      // case  0: ev.event = EVENT_SYSCALL; break; // exit
+      // case  1: ev.event = EVENT_SYSCALL; break; // yield
+      // case  4: ev.event = EVENT_SYSCALL; break; // write
     default: ev.event = EVENT_ERROR; break;
     }
 
