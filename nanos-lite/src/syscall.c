84a030f54c7055fcff24bc9c1f41c66fd24b7b84
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 17:46:20 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:46:20 up 6 days, 18:03,  2 users,  load average: 0.23, 0.14, 0.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,6 +8,7 @@ void do_syscall(Context* c) {
   switch (a[0]) {
   case -1:break;
   case SYS_yield:printf("SYS_yield\n"); yield();; break; //c->GPRx = 0
+  case SYS_exit:halt(0);
   default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
