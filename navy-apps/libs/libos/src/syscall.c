a731da7814d4911ca27a9341cae21ffbb7aad63a
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 23:21:27 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:21:27 up 6 days, 23:38,  2 users,  load average: 0.42, 0.40, 0.25

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -74,7 +74,7 @@ void* _sbrk(intptr_t increment) {
   char* prev_heap_end;
 
   if (0 == heap_end) {
-    heap_end = end;			/* Initialize first time round */
+    heap_end = &end;			/* Initialize first time round */
   }
 
   prev_heap_end = heap_end;
