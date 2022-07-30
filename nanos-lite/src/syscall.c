ccf1889dd9fd874f470390dd369a7c9b4e15086f
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 17:48:23 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:48:23 up 6 days, 18:05,  2 users,  load average: 0.36, 0.22, 0.13

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,10 +4,10 @@ void do_syscall(Context* c) {
   uintptr_t a[4];
   // 得到系统调用号
   a[0] = c->GPR1;
-
+  panic(" syscall ID = %d", a[0]);
   switch (a[0]) {
   case -1:break;
-  case SYS_yield:printf("SYS_yield\n"); yield();; break; //c->GPRx = 0
+  case SYS_yield:printf("SYS_yield\n"); yield();c->GPRx = 0; break;
   case SYS_exit:halt(0);
   default: panic("Unhandled syscall ID = %d", a[0]);
   }
