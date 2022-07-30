5f36850a55dafcc3b4a2a7d88c09d2c5ef593913
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 23:43:02 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:43:02 up 19 min,  1 user,  load average: 0.25, 0.47, 0.37

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -5,12 +5,21 @@ int main() {
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
-  while (1) {
-    j ++;
-    if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
-      j = 0;
-    }
-  }
+  extern char etext, edata, end; /* The symbols must have some type,
+                                 or "gcc -Wall" complains */
+                                 // while (1) {
+                                 //   j++;
+                                 //   if (j == 10000) {
+                                 //     printf("Hello World from Navy-apps for the %dth time!\n", i++);
+                                 //     j = 0;
+                                 //   }
+                                 // }
+
+  printf("First address past:\n");
+  printf("    program text (etext)      %10p\n", &etext);
+  printf("    initialized data (edata)  %10p\n", &edata);
+  printf("    uninitialized data (end)  %10p\n", &end);
+
   return 0;
+
 }
