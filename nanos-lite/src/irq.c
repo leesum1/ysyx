e15be5f47d4d5311231104f1970fe13fea27ccdf
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 16:01:28 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:01:28 up 6 days, 16:18,  3 users,  load average: 0.37, 0.27, 0.17

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,10 +8,15 @@
 //   EVENT_IRQ_TIMER, EVENT_IRQ_IODEV,
 // };
 
+extern void do_syscall(Context* c);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
   case EVENT_YIELD: printf("do_event:EVENT_YIELD\n");break;
-  case EVENT_SYSCALL: printf("do_event:EVENT_SYSCALL\n");break;
+  case EVENT_SYSCALL:
+    printf("do_event:EVENT_SYSCALL\n");
+    do_syscall(c);
+    break;
   default: panic("Unhandled event ID = %d", e.event);
   }
   return c;
