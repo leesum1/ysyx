93be17db23061a78f5619e41d00f5fcb75653cd5
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 23:17:42 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:17:42 up 6 days, 23:34,  2 users,  load average: 0.54, 0.25, 0.17

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -68,14 +68,14 @@ int _write(int fd, void* buf, size_t count) {
 }
 
 void* _sbrk(intptr_t increment) {
-  extern char __heap_start;//set by linker
+  extern  end;//set by linker
   extern char __heap_end;//set by linker
 
   static char* heap_end;		/* Previous end of heap or 0 if none */
   char* prev_heap_end;
 
   if (0 == heap_end) {
-    heap_end = &__heap_start;			/* Initialize first time round */
+    heap_end = end;			/* Initialize first time round */
   }
 
   prev_heap_end = heap_end;
@@ -89,11 +89,9 @@ void* _sbrk(intptr_t increment) {
   //   return (char*)-1;
   // }
   // return (void*)prev_heap_end;
-
   if (_syscall_(SYS_brk, heap_end, 0, 0) != 0) {
     return (char*)-1;
   }
-
   return (void*)prev_heap_end;
 }
 
