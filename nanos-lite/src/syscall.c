718ac674af005d41b1278081f3f172fc89f1bab2
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-04 16:44:13 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:44:13 up 4 days, 16:38,  1 user,  load average: 0.26, 0.29, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -61,7 +61,7 @@ void do_syscall(Context* c) {
     break;
   case SYS_open:
 #ifdef STRACE
-    printf("SYS_open a1:%d,a2:%p,a3:%d\n", a[1], a[2], a[3]);
+    printf("SYS_open a1:%s,a2:%p,a3:%d\n", a[1], a[2], a[3]);
 #endif
     c->GPRx = fs_open((const char*)a[1], a[2], a[3]);
     break;
