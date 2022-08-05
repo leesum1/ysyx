6da600a4bd6dd99ed8f5f66b386fd4fd99d8aaa1
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-05 23:18:29 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:18:29 up 5 days, 23:13,  2 users,  load average: 0.70, 0.49, 0.33

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -5,12 +5,12 @@
 #include <BMP.h>
 
 int main() {
+  printf("BMP_Load ends! Spinning...\n");
   NDL_Init(0);
   int w, h;
   w = 0;
   h = 0;
   void* bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
-  printf("BMP_Load ends! Spinning...\n");
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
   printf("w:%d,h:%d\n", w, h);
