26c5b3c0b8f67d0c10bce5c80e8ab199ec722e6e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 17:10:35 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:10:35 up 6 days, 17:05,  2 users,  load average: 1.00, 0.47, 0.40

--- navy-apps/libs/libbmp/src/BMP.c
+++ navy-apps/libs/libbmp/src/BMP.c
@@ -44,7 +44,7 @@ void* BMP_Load(const char* filename, int* width, int* height) {
       pixels[w * i + j] = (r << 16) | (g << 8) | b;
     }
   }
-  // fclose(fp);
+  fclose(fp);
   if (width) *width = w;
   if (height) *height = h;
   return pixels;
