bca2815da5f8a7b029d3031fb767942038b1d759
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-07 23:33:07 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:33:07 up 7 days, 23:27,  2 users,  load average: 1.08, 0.97, 1.15

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -64,7 +64,7 @@ static void prev() {
 }
 
 static void clear_display(void) {
-  SDL_FillRect(screen, NULL, 0xffffff);
+  SDL_FillRect(screen, NULL, 0x00);
 }
 
 int main(int argc, char* argv[], char* envp[]) {
