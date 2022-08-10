40bdba704e89fe6a06442d7852bbd071c32c07b8
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 17:23:01 +0800

    > sim RTL ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:23:01 up 10 days, 17:17,  2 users,  load average: 0.60, 0.75, 0.98

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -6,7 +6,7 @@ extern char _heap_start;
 int main(const char* args);
 
 extern char _pmem_start;
-#define PMEM_SIZE (128 * 1024 * 1024)
+#define PMEM_SIZE (512 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 Area heap = RANGE(&_heap_start, PMEM_END);
