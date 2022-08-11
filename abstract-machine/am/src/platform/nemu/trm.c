dfb43f99cb8a334bcf3ee12cf899413dd063cfa7
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 18:54:52 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux leesum-ubuntu 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:54:52 up 23 min,  1 user,  load average: 0.44, 0.72, 0.62

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -2,13 +2,13 @@
 #include <nemu.h>
 
 extern char _heap_start;
-int main(const char *args);
+int main(const char* args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
-static const char mainargs[] = MAINARGS;
+//static const char mainargs[] = MAINARGS;
 
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
@@ -22,6 +22,6 @@ void halt(int code) {
 }
 
 void _trm_init() {
-  int ret = main(mainargs);
+  int ret = main("t");
   halt(ret);
 }
