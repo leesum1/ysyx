36032f0236efa28899747aa38ead44ca692bba94
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 22:31:01 +0800

    > compile NEMU ysyx_22041514 李子龙 Linux aly 5.4.0-122-generic #138-Ubuntu SMP Wed Jun 22 15:00:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:31:01 up 12 days, 22:25,  1 user,  load average: 0.29, 0.32, 0.63

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -11,6 +11,7 @@ WORK_DIR  = $(shell pwd)
 BUILD_DIR = $(WORK_DIR)/build
 
 INC_PATH := $(WORK_DIR)/include $(INC_PATH)
+INC_PATH += /usr/include/SDL2
 OBJ_DIR  = $(BUILD_DIR)/obj-$(NAME)$(SO)
 BINARY   = $(BUILD_DIR)/$(NAME)$(SO)
 
